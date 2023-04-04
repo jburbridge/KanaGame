182c3cc4391dec617101534d347f0019821c5508
Josh Burbridge <joshuaburbridge@gmail.com>
2023-04-03 21:14:09 -0500

    Clean up a few things

--- scripts/main.js
+++ scripts/main.js
@@ -1,51 +1,47 @@
-function nextKana(code)
-{
+function nextKana(code) {
 	document.getElementById('message').innerHTML = "What sound does this character make?";
 	document.getElementById('userAnswer').value = "";
 	var kana = document.getElementById("kana");
 
 	//If it's a monograph
-	if(code.length < 5)
+	if(code.length < 5) {
 		kana.innerHTML = String.fromCharCode(parseInt(code, 16));
+	}
 
 	//Else, it's a digraph, so split the two codes apart and print each
-	else
-	{
+	else {
 		var kana1 = String.fromCharCode(parseInt(code.substring(0, 4), 16));
 		var kana2 = String.fromCharCode(parseInt(code.substring(5, 9), 16))
 		kana.innerHTML = kana1 + kana2;
 	}
 }
 
-function checkAnswer(actualAnswer)
-{
+function checkAnswer(actualAnswer) {
 	var userAnswer = document.getElementById('userAnswer');
 	var message = document.getElementById('message');
 
 	//If they submitted a blank answer, don't respond
-	if(userAnswer.value.length === 0)
+	if(userAnswer.value.length === 0) {
 		return;
+	}
 
 	//Right answer
-	else if (userAnswer.value == actualAnswer)
+	else if (userAnswer.value == actualAnswer) {
 	 	message.innerHTML = "Correct! It's " + actualAnswer + "!";
+	}
 
 	//Wrong answer
-	else
-	{
+	else {
 	 	message.innerHTML = "Nope, it's not " + userAnswer.value + ".";
-
 	}
 	userAnswer.value = "";
 }
 
-function showAnswer(actualAnswer)
-{
+function showAnswer(actualAnswer) {
 	document.getElementById('message').innerHTML = "Answer: " + actualAnswer;
 }
 
-function readTextFile(file)
-{
+function readTextFile(file) {
 	var codes = [];
 	var romaji = [];
 
@@ -54,37 +50,32 @@ function readTextFile(file)
 
     var rawFile = new XMLHttpRequest();
     rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+
                 var allText = rawFile.responseText;
                 splitText = allText.split(/ |\r\n/);
 
-				if(learnHiragana)
-				{
+				if(learnHiragana) {
+
 					//71 hiragana monographs
-					for(var i = 0; i < 142; i+=2)
-					{
+					for(var i = 0; i < 142; i+=2) {
 						codes.push(splitText[i]);
 						romaji.push(splitText[i+1]);
 					}
 
 					//36 hiragana digraphs
-					for(var i = 142; i < 250; i+=3)
-					{
+					for(var i = 142; i < 250; i+=3) {
 						codes.push(splitText[i] + '-' + splitText[i+1]);
 						romaji.push(splitText[i+2]);
 					}
 				}
 
-				if(learnKatakana)
-				{
+				if(learnKatakana) {
+
 					//71 katakana monographs
-					for(var i = 250; i < 392; i+=2)
-					{
+					for(var i = 250; i < 392; i+=2) {
 						codes.push(splitText[i]);
 						romaji.push(splitText[i+1]);
 					}
@@ -112,16 +103,17 @@ document.getElementById("userAnswer").focus();
 input.addEventListener("keydown", function(event) {
 
 	//Enter: submit answer
-	if(event.which === 13)
+	if(event.which === 13) {
 		checkAnswer(romaji[index]);
+	}
 
 	//Shift: show answer
-	else if(event.which === 16)
+	else if(event.which === 16) {
 		showAnswer(romaji[index]);
+	}
 
 	//Space next kana
-	else if(event.which === 32)
-	{
+	else if(event.which === 32) {
 		index = Math.floor(Math.random() * codes.length);
 		nextKana(codes[index]);
 	}
@@ -129,6 +121,7 @@ input.addEventListener("keydown", function(event) {
 
 //Sloppy way of clearing input buffer
 input.addEventListener("keyup", function(event) {
-	if(event.which === 32)
+	if(event.which === 32) {
 		this.value = "";
+	}
 });
